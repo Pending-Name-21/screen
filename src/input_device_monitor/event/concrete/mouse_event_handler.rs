a783a7f1e7cfd5b2827433225a36b7f84d4d639c
luiggy2620 <luiggymamani@gmail.com>
2024-06-07 18:35:14 -0400

    test: adding mouse event handler unit tests

--- src/input_device_monitor/event/concrete/mouse_event_handler.rs
+++ src/input_device_monitor/event/concrete/mouse_event_handler.rs
@@ -1,5 +1,5 @@
-use std::sync::{Arc, Mutex};
 use nannou::event::WindowEvent;
+use std::sync::{Arc, Mutex};
 
 use crate::input_device_monitor::{event::AEventHandler, sender::IEventSender};
 
@@ -20,12 +20,75 @@ impl AEventHandler for MouseEventHandler {
             | WindowEvent::MousePressed(_)
             | WindowEvent::MouseReleased(_)
             | WindowEvent::MouseEntered
-            | WindowEvent::MouseExited
-            | WindowEvent::MouseWheel(_, _) => {
+            | WindowEvent::MouseExited => {
                 let sender = self.sender.lock().unwrap();
                 sender.send_event(event);
             }
             _ => {}
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nannou::{
+        event::{MouseButton, WindowEvent},
+        glam::Vec2,
+    };
+
+    struct MockMouseEvent;
+
+    impl IEventSender for MockMouseEvent {
+        fn send_event(&self, event: &WindowEvent) {
+            match event {
+                WindowEvent::MouseMoved(event) => assert_eq!(*event, Vec2::new(0.0, 0.0)),
+                WindowEvent::MousePressed(event) => assert_eq!(*event, MouseButton::Left),
+                WindowEvent::MouseReleased(event) => assert_eq!(*event, MouseButton::Left),
+                WindowEvent::MouseEntered => assert_eq!(*event, WindowEvent::MouseEntered),
+                WindowEvent::MouseExited => assert_eq!(*event, WindowEvent::MouseExited),
+                _ => panic!("Unexpected mouse event"),
+            }
+        }
+    }
+
+    #[test]
+    fn test_handle_mouse_moving() {
+        let sender: Arc<Mutex<dyn IEventSender + Send>> = Arc::new(Mutex::new(MockMouseEvent));
+        let handler = MouseEventHandler::new(sender);
+        let mouse_event = WindowEvent::MouseMoved(Vec2::new(0.0, 0.0));
+        handler.handle_event(&mouse_event);
+    }
+
+    #[test]
+    fn test_handle_mouse_pressed() {
+        let sender: Arc<Mutex<dyn IEventSender + Send>> = Arc::new(Mutex::new(MockMouseEvent));
+        let handler = MouseEventHandler::new(sender);
+        let mouse_event = WindowEvent::MousePressed(MouseButton::Left);
+        handler.handle_event(&mouse_event);
+    }
+
+    #[test]
+    fn test_handle_mouse_released() {
+        let sender: Arc<Mutex<dyn IEventSender + Send>> = Arc::new(Mutex::new(MockMouseEvent));
+        let handler = MouseEventHandler::new(sender);
+        let mouse_event = WindowEvent::MouseReleased(MouseButton::Left);
+        handler.handle_event(&mouse_event);
+    }
+
+    #[test]
+    fn test_handle_mouse_entered() {
+        let sender: Arc<Mutex<dyn IEventSender + Send>> = Arc::new(Mutex::new(MockMouseEvent));
+        let handler = MouseEventHandler::new(sender);
+        let mouse_event = WindowEvent::MouseEntered;
+        handler.handle_event(&mouse_event);
+    }
+
+    #[test]
+    fn test_handle_mouse_exited() {
+        let sender: Arc<Mutex<dyn IEventSender + Send>> = Arc::new(Mutex::new(MockMouseEvent));
+        let handler = MouseEventHandler::new(sender);
+        let mouse_event = WindowEvent::MouseExited;
+        handler.handle_event(&mouse_event);
     }
 }
