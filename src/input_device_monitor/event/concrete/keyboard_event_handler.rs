922123a6c3e4738cedef9727dd2ca31684575f61
luiggy2620 <luiggymamani@gmail.com>
2024-06-07 17:53:36 -0400

    refactor: event handler separated by modules like the uml

--- src/input_device_monitor/event/concrete/keyboard_event_handler.rs
+++ src/input_device_monitor/event/concrete/keyboard_event_handler.rs
@@ -1,25 +1,27 @@
+use std::sync::{Arc, Mutex};
 use nannou::event::WindowEvent;
-use crate::input_device_monitor::sender::ISender;
 use crate::input_device_monitor::event::AEventHandler;
+use crate::input_device_monitor::sender::IEventSender;
 
 pub struct KeyboardEventHandler {
-    sender: Box<dyn ISender>,
+    pub sender: Arc<Mutex<dyn IEventSender + Send>>,
 }
 
 impl KeyboardEventHandler {
-    pub fn new(sender: Box<dyn ISender>) -> Self {
+    pub fn new(sender: Arc<Mutex<dyn IEventSender + Send>>) -> Self {
         Self { sender }
     }
 }
 
 impl AEventHandler for KeyboardEventHandler {
     fn handle_event(&self, event: &WindowEvent) {
-        if let WindowEvent::KeyPressed(key) = event {
-            println!("Key pressed: {:?}", key);
-            self.sender.send_event(event);
+        if let WindowEvent::KeyPressed(_) = event {
+            let sender = self.sender.lock().unwrap();
+            sender.send_event(event);
         }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -27,7 +29,7 @@ mod tests {
     use nannou::event::{WindowEvent, Key};
 
     struct MockSender;
-    impl ISender for MockSender {
+    impl IEventSender for MockSender {
         fn send_event(&self, event: &WindowEvent) {
             match event {
                 WindowEvent::KeyPressed(key) => assert_eq!(*key, Key::A),
@@ -38,7 +40,8 @@ mod tests {
 
     #[test]
     fn test_handle_key_pressed() {
-        let sender = Box::new(MockSender);
+        let sender: Arc<Mutex<dyn IEventSender + Send>> 
+            = Arc::new(Mutex::new(MockSender));
         let handler = KeyboardEventHandler::new(sender);
         let key_event = WindowEvent::KeyPressed(Key::A);
         handler.handle_event(&key_event);
---
+++ src/input_device_monitor/event/concrete/mod.rs
@@ -0,0 +1,3 @@
+pub mod keyboard_event_handler;
+
+pub use keyboard_event_handler::KeyboardEventHandler;
