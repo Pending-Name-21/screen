929f1024f39f1c2a871637452c405c976d9689ec
luiggy2620 <luiggymamani@gmail.com>
2024-06-14 19:39:53 -0400

    ci: added caster for screen event representation

--- src/input_device_monitor/my_event/mod.rs
+++ src/input_device_monitor/my_event/mod.rs
@@ -1,4 +1,9 @@
 pub mod key;
 pub mod mouse_button;
-pub mod window;
 pub mod vec2;
+pub mod window;
+
+pub use key::MyKey;
+pub use mouse_button::MyMouseButton;
+pub use vec2::MyVec2;
+pub use window::MyWindowEvent;
