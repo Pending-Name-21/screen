0b2c74e23ace4a5d8e012c7dbe6386baabf915b3
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2024-06-14 23:17:14 -0400

    feat: file .env added for credentials

--- src/socket_server/server.rs
+++ src/socket_server/server.rs
@@ -3,9 +3,13 @@ use crossbeam_channel::Sender;
 use std::io::BufReader;
 use std::net::TcpListener;  
 use std::thread;
+use dotenv::dotenv;
+use std::env;
 
 pub fn run_server(tx: Sender<Frame>) {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    dotenv().ok();
+    let address: String = env::var("ADDRESS").expect("ADDRESS must be set");
+    let listener = TcpListener::bind(address).unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
